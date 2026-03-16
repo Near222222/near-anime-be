290147bca26123d07a902c0de480fc182d2cfa0c
Near111111 <Jomarmerej@gmail.com>
2026-03-16 19:10:50 +0800

    Fix: servers and stream working

--- src/controllers/servers.controller.js
+++ src/controllers/servers.controller.js
@@ -2,8 +2,8 @@ import { extractServers } from "../extractors/streamInfo.extractor.js";
 
 export const getServers = async (req) => {
   try {
-    const { ep } = req.query;
-    const servers = await extractServers(ep);
+    const id = req.params.id || req.query.ep;
+    const servers = await extractServers(id);
     return servers;
   } catch (e) {
     console.error(e);
--- src/extractors/streamInfo.extractor.js
+++ src/extractors/streamInfo.extractor.js
@@ -8,14 +8,15 @@ import { decryptSources_v1 } from "../parsers/decryptors/decrypt_v1.decryptor.js
 export async function extractServers(id) {
   try {
     const resp = await axios.get(
-      `https://${v1_base_url}/ajax/v2/episode/servers?episodeId=${id}`
+      `https://${v1_base_url}/ajax/v2/episode/servers?episodeId=${id}`,
     );
-    const $ = cheerio.load(resp.data.html);
+    const html = resp.data?.html ?? resp.data;
+    const $ = cheerio.load(html);
     const serverData = [];
     $(".server-item").each((index, element) => {
-      const data_id = $(element).attr("data-id");
-      const server_id = $(element).attr("data-server-id");
-      const type = $(element).attr("data-type");
+      const data_id = $(element).attr("data-id")?.trim();
+      const server_id = $(element).attr("data-server-id")?.trim();
+      const type = $(element).attr("data-type")?.trim();
 
       const serverName = $(element).find("a").text().trim();
       serverData.push({
@@ -38,18 +39,18 @@ async function extractStreamingInfo(id, name, type, fallback) {
     let requestedServer = servers.filter(
       (server) =>
         server.serverName.toLowerCase() === name.toLowerCase() &&
-        server.type.toLowerCase() === type.toLowerCase()
+        server.type.toLowerCase() === type.toLowerCase(),
     );
     if (requestedServer.length === 0) {
       requestedServer = servers.filter(
         (server) =>
           server.serverName.toLowerCase() === name.toLowerCase() &&
-          server.type.toLowerCase() === "raw"
+          server.type.toLowerCase() === "raw",
       );
     }
     if (requestedServer.length === 0) {
       throw new Error(
-        `No matching server found for name: ${name}, type: ${type}`
+        `No matching server found for name: ${name}, type: ${type}`,
       );
     }
     const streamingLink = await decryptSources_v1(
@@ -57,7 +58,7 @@ async function extractStreamingInfo(id, name, type, fallback) {
       requestedServer[0].data_id,
       name,
       type,
-      fallback
+      fallback,
     );
     return { streamingLink, servers };
   } catch (error) {
